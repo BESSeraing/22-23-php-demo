0155165a93e9592c805858beb42926de237823ca
Jona <jona303@gmail.com>
2023-02-03 15:36:38 +0100

    add menu exemple

--- exercices/nav.php
+++ exercices/nav.php
@@ -15,7 +15,10 @@ displayNav($pages);
 function displayNav(array $pages) {
     echo '<nav>
         <ul>';
-// Faites une boucle sur ce tableau pour écrire le menu
+    echo '<li><a href="?id=1">Accueil</a></li>';
+    echo '<li><a href="?id=2">Contact</a></li>';
+    echo '<li><a href="?id=3">Réservations</a></li>';
+// Faites une boucle sur ce tableau pour écrire le menu parce que la méthode utilisée ci dessus n'est pas acceptable
 
 echo '</ul>
 </nav>';
